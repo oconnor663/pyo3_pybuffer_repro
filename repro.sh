a1cc428499fb8f7ce708401595effae326775e5e
Jack O'Connor <jack.oconnor@zoom.us>
2021-07-21 15:36:20 -0400

    add repro.sh

---
+++ repro.sh
@@ -0,0 +1,14 @@
+#! /usr/bin/env bash
+
+set -e -u -o pipefail
+
+venv_dir="$(mktemp -d)"
+virtualenv -p pypy3 "$venv_dir"
+source "$venv_dir/bin/activate"
+maturin develop
+
+i=1
+while python ./test.py ; do
+    echo "$i"
+    i=$(($i + 1))
+done
